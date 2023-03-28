c71b28030a98665e966d26072d9d35ff45dcaa91
Anton.Gorynia <antongorynya1@gmail.com>
2023-03-28 14:52:30 +0300

    fix migrations

--- property/migrations/0003_auto_20230328_1217.py
+++ property/migrations/0003_auto_20230328_1217.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0001_initial'),
+        ('property', '0002_auto_20190829_2242'),
     ]
 
     operations = [

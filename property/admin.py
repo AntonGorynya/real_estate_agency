4ba0cfd68664649bc81aa8845af8b14b03d8eeec
Anton.Gorynia <antongorynya1@gmail.com>
2023-03-28 12:23:59 +0300

    add address field to Flat changelist page.

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Flat
 class Admin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['price', 'new_building', 'construction_year']
+    list_display = ['address', 'price', 'new_building', 'construction_year']
     list_editable = ['new_building']
 
 
